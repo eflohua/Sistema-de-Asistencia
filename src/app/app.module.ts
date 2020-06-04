a3041992a6359331c23f02a7be0de0afd9f52584
TheDenky <denkyruben@gmail.com>
2020-06-04 00:55:52 -0400

    Cdn bootstrap, servidor, navegador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegadorComponent } from './components/navegador/navegador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegadorComponent
   ],
   imports: [
     BrowserModule,
