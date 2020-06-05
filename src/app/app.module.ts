f74a20f5cc9fa6aa3877f3174e504c19acc3cf63
Ivan <dm1van2t.t16@gmail.com>
2020-06-05 18:19:55 -0500

    formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegadorComponent } from './components/navegador/navegador.component';
+import { PersonalFormComponent } from './personal/personal-form/personal-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegadorComponent
+    NavegadorComponent,
+    PersonalFormComponent
   ],
   imports: [
     BrowserModule,
