f75694d68cc9e76892cc0d531e794a4a062f5c46
unknown <mariobrosrio57@gmail.com>
2020-06-05 00:43:45 -0500

    Login V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegadorComponent } from './components/navegador/navegador.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegadorComponent
+    NavegadorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
