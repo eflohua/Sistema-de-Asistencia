8bae1bd5b8124f97b02baa8c98f35c13fb29fa05
TheDenky <denkyruben@gmail.com>
2020-06-12 16:11:18 -0400

    datatable-personal v2

--- src/app/components/personal/personal.component.ts
+++ src/app/components/personal/personal.component.ts
@@ -9,11 +9,18 @@ export interface PeriodicElement {
   Nombres: string;
   apepat: string;
   apemat: string;
-  Jornada: string;
+  Jornada: number;
   Contrato: string;
   Cargo: string;
 }
-const ELEMENT_DATA: PeriodicElement[] = [];
+const ELEMENT_DATA: PeriodicElement[] = [
+  {DNI: '31254532', Nombres: 'Oreja', apepat: 'Flores', apemat: 'Infanzon', Jornada: 30, Contrato: 'Contratado', Cargo: 'Docente'},
+  {DNI: '42565215', Nombres: 'Paolo', apepat: 'Guerrero', apemat: 'Gonzales', Jornada: 20, Contrato: 'Nombrado', Cargo: 'Docente'},
+  {DNI: '75412356', Nombres: 'Ollanta', apepat: 'Humala', apemat: 'Tasso', Jornada: 40, Contrato: 'Nombrado', Cargo: 'Director'},
+  {DNI: '36952849', Nombres: 'Pedro Pablo', apepat: 'Kuckcynski', apemat: 'Pampañaupa', Jornada: 40, Contrato: 'Nombrado', Cargo: 'Docente'},
+  {DNI: '96352452', Nombres: 'Keyko', apepat: 'Higuchi', apemat: 'Caceres', Jornada: 32, Contrato: 'Contratado', Cargo: 'Auxiliar'},
+  {DNI: '85246970', Nombres: 'Kenyi', apepat: 'Higuchi', apemat: 'Caceres', Jornada: 26, Contrato: 'Contratado', Cargo: 'Psicologo'},
+];
 
 @Component({
   selector: 'app-personal',
