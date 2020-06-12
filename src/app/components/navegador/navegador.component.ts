d618d792103f9dd8cd6b607c206e8a00459d5a01
Ivan <dm1van2t.t16@gmail.com>
2020-06-12 10:56:19 -0500

    datatable

--- src/app/components/navegador/navegador.component.ts
+++ src/app/components/navegador/navegador.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-navegador',
--- src/app/components/personal/personal.component.ts
+++ src/app/components/personal/personal.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
+export interface PeriodicElement {
+  DNI: string;
+  Nombres: string;
+  apepat: string;
+  apemat: string;
+  Jornada: string;
+  Contrato: string;
+  Cargo: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [{DNI: '112345', Nombres: 'Juan Espinoza Medrano', apepat: 'Av. Belen de Osma', apemat: '542012', Cargo: 'Secundaria', Contrato: 'EBR', Jornada: 'Mañana'}];
 
 @Component({
   selector: 'app-personal',
@@ -7,9 +22,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalComponent implements OnInit {
 
+  displayedColumns: string[]  = ['DNI', 'Nombres', 'apepat', 'apemat', 'Cargo', 'Contrato', 'Jornada', 'ACTION'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+    
+
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
-
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
