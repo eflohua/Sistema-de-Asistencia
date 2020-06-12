26ac556563fbff8cffa05e72ddd6bf523cf2b76a
TheDenky <denkyruben@gmail.com>
2020-06-11 23:57:18 -0400

    Datatable-Institucion Corriendo

--- src/app/components/institucion/institucion.component.ts
+++ src/app/components/institucion/institucion.component.ts
@@ -5,19 +5,25 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  custom: string;
+  codModular: string;
+  nombre: string;
+  direccion: string;
+  numero: string;
+  nivel: string;
+  modalidad: string;
+  turno: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', custom: 'sf'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', custom: 'sf'},
+  {codModular: '112345', nombre: 'Juan Espinoza Medrano', direccion: 'Av. Belen de Osma', numero: '542012', nivel: 'Secundaria', modalidad: 'EBR', turno: 'Mañana'},
+  {codModular: '562143', nombre: 'Guillermo Pinto Ismodes', direccion: 'Jr. Ramon Castilla', numero: '425312', nivel: 'Secundaria', modalidad: 'EBR', turno: 'Mañana'},
+  {codModular: '245812', nombre: 'Belen de Osma y Pardo', direccion: 'Av. Juan espinoza Medrano', numero: '542123', nivel: 'Secundaria', modalidad: 'EBR', turno: 'Tarde'},
+  {codModular: '452153', nombre: 'Trilce', direccion: 'Av. jose Maria', numero: '550012', nivel: 'Primaria', modalidad: 'EBR', turno: 'Mañana'},
+  {codModular: '554554', nombre: 'Agropecuario numero 2', direccion: 'Av. San Jeronimo', numero: '58745', nivel: 'Secundaria', modalidad: 'CEBA', turno: 'Mañana'},
+  {codModular: '532012', nombre: 'Nuestra Señora de Fatima', direccion: 'Av. Tupac Amaru', numero: '54210', nivel: 'Primaria', modalidad: 'EBR', turno: 'Mañana'},/**
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', custom: 'sf'},
   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', custom: 'sf'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', custom: 'sf'},/** 
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', custom: 'sf'}, 
   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
@@ -42,7 +48,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class InstitucionComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'custom'];
+  displayedColumns: string[] = ['codModular', 'nombre', 'direccion', 'numero', 'nivel', 'modalidad', 'turno', 'ACTION'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
